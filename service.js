36073e611b7198b8be64938b06d4519cad014bad
Venpresath <Venpresath@Live.com>
2018-12-05 01:24:36 -0500

    updated content, and things worked on

--- service.js
+++ service.js
@@ -2,20 +2,10 @@
 {
     function service() {
         let post = [{
-            title: "Movie DB Project",
+            title: "Handgrown Jobs",
             img: "http://via.placeholder.com/350x150",
-            description: "Movie database website created with AngularJS, used a movie API database for the results",
-            link: "https://venpresath.github.io/Angular-Project/"
-        },{
-            title: "World's Best Workbench",
-            img: "http://via.placeholder.com/350x150",
-            description: "Small business website that I am currently working on. Utilizing AngularJS",
-            link: ""
-        },{
-            title: "Roggit",
-            img: "http://via.placeholder.com/350x150",
-            description: "Simple Reddit clone website with jQuery",
-            link: "https://venpresath.github.io/reddit-clone/"
+            description: "Cannabis career website created with React, SCSS, and Meteor",
+            link: "https://handgrown.jobs/home"
         }];
 
         let musicPost = [{
